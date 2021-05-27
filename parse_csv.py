ebd0eace83966b99b49324d4f9095a9390d04b4c
Tylor Jilk <tjilk2663@gmail.com>
2021-05-26 22:38:03 -0700

    Fixing phone number method to work for all current data

--- parse_csv.py
+++ parse_csv.py
@@ -14,15 +14,19 @@ class DocumentURL:
     year: int
     link: str
 
-def GetPhoneFromStaffInfo(staff_info):
-    '''Finds the phone number from a staff_info string and returns it as an int
+def GetPhoneFromStaffInfo(staff_info: str):
+    '''Finds the phone number from a staff_info string and returns it as a string, with the extension if available
+        - Returns None if no phone number available
+        - Returns only the first phone number if multiple are found
     '''
-    numbers = re.compile(r'\d+(?:\.\d+)?')
+    numbers = re.compile(r'(?:\(| |-|x|\n)(\d+)')
     phone = numbers.findall(staff_info)
-    if len(phone) == 3:
-        return "".join([str(num) for num in phone])
-    elif len(phone) == 4:
-        return "".join([str(num) for num in phone[0:3]]) + " x" + phone[3]
+    if len(phone) == 0:
+        return None
+    if len(phone)%3 == 0:
+        return "".join([str(num) for num in phone[0:3]])
+    elif len(phone)%4 == 0:
+        return "".join([str(num) for num in phone[0:3]]) + " x" + str(phone[3])
     else:
         return None
 
@@ -56,7 +60,9 @@ def main():
     data["lhmp_link"] = None
     data["climate_change_ack_in_plan"] = None
     data["sb378_sb1035_compliant"] = data["Updated General Plan per SB 379 & SB1035?"].str.strip()
-    print(data['phone'])
+    # for city_name, number in zip(data['city_name'], data['phone']):
+    #     if city_name == "County of Mono":
+    #         print(city_name, number)
 
 if __name__ == "__main__":
     main()
