a0340ad33e88d9978819312460b4487e14438afb
Tylor Jilk <tjilk2663@gmail.com>
2021-06-02 00:35:24 -0700

    Additional parsing functions from staff info; incorporate FEMA data

--- constants.py
+++ constants.py
@@ -2,7 +2,9 @@
 REGEX_MATCHING_FNS = {
     "url" : r"(?i)\b((?:https?:(?:\/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:.,<>?«»])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b\/?(?!@)))",
     "document" : r"(.*) \((\d+|N\/A)\)",
-    "phone_number" : r"(?:\(| |-|x|\n)(\d+)"
+    "phone_number" : r"(?:\(| |-|x|\n)(\d+)",
+    "email" : r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",
+    "contact_name_position" : r"([a-zA-Z]+) ([a-zA-Z]+), ([a-zA-Z ]*?)(?:\n| *\n)"
 }
 
 # Mapping of plan type (abbreviation) to a list of possible words used to describe it
@@ -16,6 +18,12 @@ PLAN_TYPE_MAP = {
 CLIMATE_RESOLVE_META = {
     "excel_fname": "Matrix 1.1 - Status of Municipal Climate Preparedness.xlsx",
     "excel_tabname": "1. Summary"
+}
+
+# Dictionary of useful metadata about the FEMA spreadsheet
+FEMA_META = {
+    "excel_fname": "FEMA_bay_area_so_cal_LHMP_plan_status_10-26-20.xlsx",
+    "excel_tabname": "Sheet1"
 }
 
 # Mapping of column names from the Climate Resolve matrix to the output excel file
@@ -34,6 +42,44 @@ CLIMATE_RESOLVE_COLUMN_TO_OUTPUT_COLUMN_MAP = {
     "Notes": "notes"
 }
 
+# Mapping of column names from the FEMA data to the output excel file
+FEMA_COLUMN_TO_OUTPUT_COLUMN_MAP = {
+    "County Name": "county_name_fema",
+    "Community Name": "municipality_name_fema", 
+    "Plan Status": "lhmp_status",
+    "Plan Type": "lhmp_type",
+    "Plan is an Update (Flag)": "lhmp_update_flag",
+    "Plan Update Number": "lhmp_update_number",
+    "Plan Approval Date": "lhmp_approval_date",
+    "Expiration Date": "lhmp_expiration_date"
+}
+
+# Mapping of Climate Resolve municipality names to corresponding FEMA names if not matched
+CLIMATE_RESOLVE_TO_FEMA_MUNICIPALITY_MAPPING = {
+    # "County of Fresno": ,
+    "County of Imperial": "Imperial County",
+    # "County of Inyo": ,
+    # "County of Kern": ,
+    # "County of Kings": ,
+    "County of Los Angeles": "Los Angeles County",
+    # "County of Madera": ,
+    # "County of Mono": ,
+    "County of Orange": "Orange County",
+    "County of Riverside": "Riverside County",
+    "County of San Bernardino": "San Bernardino County",
+    # "County of Santa Barbara": ,
+    # "County of Tulare": ,
+    # "County of Tuolumne": ,
+    "County of Ventura": "Ventura County",
+    "Filmore": "Fillmore",
+    "Jurupa Valley": "Jurupa Valley City",
+    "La Canada Flintridge": "La Cañada Flintridge",
+    "La Habra Heights": "La Habra",
+    "Rolling HIlls": "Rolling Hills",
+    "Yucca Valley": "Yucca Valley town"
+}
+
+# Metadata about the final output file
 OUTPUT_FILE_META = {
     "excel_fname": "cleaned_data.xlsx",
     "columns_to_exclude": [
--- parse_csv.py
+++ parse_csv.py
@@ -10,6 +10,47 @@ class DocumentURL:
     year: str
     link: str
 
+def load_climate_resolve_data():
+    '''Loads the climate resolve data and returns it as a pandas dataframe'''
+    fname = constants.CLIMATE_RESOLVE_META["excel_fname"]
+    sheet_name = constants.CLIMATE_RESOLVE_META["excel_tabname"]
+    usecols = list(constants.CLIMATE_RESOLVE_COLUMN_TO_OUTPUT_COLUMN_MAP.keys())
+    return pd.read_excel(fname, sheet_name=sheet_name, usecols=usecols)
+
+def load_fema_data():
+    '''Loads the fema data and returns it as a pandas dataframe'''
+    fname = constants.FEMA_META["excel_fname"]
+    sheet_name = constants.FEMA_META["excel_tabname"]
+    usecols = list(constants.FEMA_COLUMN_TO_OUTPUT_COLUMN_MAP.keys())
+    return pd.read_excel(fname, sheet_name=sheet_name, usecols=usecols)
+
+def FixMunicipalities(name: str):
+    if name in constants.CLIMATE_RESOLVE_TO_FEMA_MUNICIPALITY_MAPPING.keys():
+        return constants.CLIMATE_RESOLVE_TO_FEMA_MUNICIPALITY_MAPPING[name]
+    return name
+
+def parse_climate_resolve_data(data):
+    '''Extract additional information from the data'''
+    data["contact_phone"] = data["staff_info"].apply(lambda x: GetPhoneFromStaffInfo(x))
+    data["contact_email"] = data["staff_info"].apply(lambda x: GetEmailFromStaffInfo(x))
+    data["contact_first_name"], data["contact_last_name"], data["contact_title"] = zip(*data["staff_info"].apply(lambda x: GetContactNamePositionFromStaffInfo(x)))
+    data["documents"] = data["URL's to relevant documents"].apply(lambda x: GetDocumentsFromURLs(x))
+    data["cap_link"] = data["documents"].apply(lambda x: GetSpecificURL(x, constants.PLAN_TYPE_MAP["cap"]))
+    # data["sust_status"] = None # TODO
+    data["sust_link"] = data["documents"].apply(lambda x: GetSpecificURL(x, constants.PLAN_TYPE_MAP["sust"]))
+    data["lhmp_link"] = data["documents"].apply(lambda x: GetSpecificURL(x, constants.PLAN_TYPE_MAP["lhmp"]))
+    data["municipality_name"] = data["municipality_name"].apply(lambda x: FixMunicipalities(x))
+
+def remove_ending_city_from_word(word):
+    pattern = re.compile(r"([a-zA-Z ().ñ]*?) city")
+    res = pattern.findall(word)
+    if len(res) == 1:
+        return res[0]
+    return word
+
+def parse_fema_data(data):
+    data["municipality_name_fema"] = data["municipality_name_fema"].apply(lambda x: remove_ending_city_from_word(x))
+
 def GetPhoneFromStaffInfo(staff_info: str):
     '''Finds the phone number from a staff_info string and returns it as a string, with the extension if available
         - Returns None if no phone number available
@@ -19,12 +60,36 @@ def GetPhoneFromStaffInfo(staff_info: str):
     phone = numbers.findall(staff_info)
     if len(phone) == 0:
         return None
-    if len(phone)%3 == 0:
+    elif len(phone)%3 == 0:
         return "".join([str(num) for num in phone[0:3]])
     elif len(phone)%4 == 0:
         return "".join([str(num) for num in phone[0:3]]) + " x" + str(phone[3])
     else:
         return None
+
+def GetEmailFromStaffInfo(staff_info: str):
+    '''Finds the email from a staff_info string and returns it as a string
+        - Returns None if no email found
+        - Returns only the first email address if multiple are found
+    '''
+    email_fn = re.compile(constants.REGEX_MATCHING_FNS["email"])
+    emails = email_fn.findall(staff_info)
+    if len(emails) == 0:
+        return None
+    else:
+        return emails[0]
+
+def GetContactNamePositionFromStaffInfo(staff_info: str):
+    '''Finds the contact name and position from a staff_info string and returns it as a 3-tuple
+        - Returns None if not found
+        - Returns only the first if multiple are found
+    '''
+    find_fn = re.compile(constants.REGEX_MATCHING_FNS["contact_name_position"])
+    contacts = find_fn.findall(staff_info)
+    if len(contacts) == 0:
+        return (None, None, None)
+    else:
+        return contacts[0]
 
 def GetDocumentsFromURLs(urls_to_relevant_docs: str):
     '''Generates DocumentURLs from info found in the provided string
@@ -89,29 +154,30 @@ def CleanData(data: pd.DataFrame, column_map: dict):
         - Nothing
     '''
     for old_column, new_column in column_map.items():
-        data[new_column] = data[old_column].str.strip()
+        if type(data[old_column][0]) == str:
+            data[new_column] = data[old_column].str.strip()
 
 def main():
-    # Load the Climate Resolve Excel file into a pandas dataframe
-    fname = constants.CLIMATE_RESOLVE_META["excel_fname"]
-    sheet_name = constants.CLIMATE_RESOLVE_META["excel_tabname"]
-    usecols = list(constants.CLIMATE_RESOLVE_COLUMN_TO_OUTPUT_COLUMN_MAP.keys())
-    data = pd.read_excel(fname, sheet_name=sheet_name, usecols=usecols)
-
-    # Clean any white spaces, etc., from the data and map to the new column names
+    # Load the Climate Resolve Excel file into a pandas dataframe, clean it, and parse
+    data = load_climate_resolve_data()
     CleanData(data, constants.CLIMATE_RESOLVE_COLUMN_TO_OUTPUT_COLUMN_MAP)
+    parse_climate_resolve_data(data)
 
-    # Extract additional information from the data
-    data["phone"] = data["staff_info"].apply(lambda x: GetPhoneFromStaffInfo(x))
-    data["documents"] = data["URL's to relevant documents"].apply(lambda x: GetDocumentsFromURLs(x))
+    fema_data = load_fema_data()
+    CleanData(fema_data, constants.FEMA_COLUMN_TO_OUTPUT_COLUMN_MAP)
+    parse_fema_data(fema_data)
 
-    data["cap_link"] = data["documents"].apply(lambda x: GetSpecificURL(x, constants.PLAN_TYPE_MAP["cap"]))
-    # data["sust_status"] = None # TODO
-    data["sust_link"] = data["documents"].apply(lambda x: GetSpecificURL(x, constants.PLAN_TYPE_MAP["sust"]))
-    data["lhmp_link"] = data["documents"].apply(lambda x: GetSpecificURL(x, constants.PLAN_TYPE_MAP["lhmp"]))
+    # municipalities_fema = set(list(fema_data['municipality_name_fema']))
+    # municipalities_cr = set(list(data['municipality_name']))
+
+    # municipalities_cr.difference_update(municipalities_fema)
+    # print(sorted(municipalities_cr))
+
+    # Combine the Climate Resolve and FEMA data
+    data = pd.merge(data, fema_data, how="left", left_on="municipality_name", right_on="municipality_name_fema")
 
     columns = set(data.keys())
-    columns.difference_update(set(constants.CLIMATE_RESOLVE_COLUMN_TO_OUTPUT_COLUMN_MAP.keys()))
+    columns.difference_update(set(list(constants.CLIMATE_RESOLVE_COLUMN_TO_OUTPUT_COLUMN_MAP.keys()) + list(constants.FEMA_COLUMN_TO_OUTPUT_COLUMN_MAP.keys())))
     columns.difference_update(set(constants.OUTPUT_FILE_META['columns_to_exclude']))
 
     data.to_excel(constants.OUTPUT_FILE_META["excel_fname"], columns=columns, index=False)
